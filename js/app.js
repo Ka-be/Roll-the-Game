0597fa6f51431169042033427a55bcbb1c4da696
Kevin Bourgitteau <kevin.bourgitteau@gmail.com>
2022-12-01 09:50:23 +0100

    add animation on player display + players Data js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,35 @@
+//PLAYERS DATA
+let players = [
+	{
+		playerNumber: 1,
+		name: "Player 1",
+		roundScore: 0,
+		totalScore: 0,
+		isTheWinner: false,
+	},
+	{
+		playerNumber: 2,
+		name: "Player 2",
+		roundScore: 0,
+		totalScore: 0,
+		isTheWinner: false,
+	},
+];
+
+
+// DISPLAY PLAYERS NAMES
+const player1name = document.getElementById('player1-name');
+const player2name = document.getElementById('player2-name');
+
+function whatIsYourName(){
+	players[0].name = prompt("Joueur 1");
+	players[1].name = prompt("Joueur 2");
+}
+// whatIsYourName();
+
+player1name.innerText = players[0].name;
+player2name.innerText = players[1].name;
+
 // RANDOM DICE NUMBER PART
 const rollButton = document.getElementById("roll"); //Button roll
 let diceDisplay = document.getElementById("diceDisplay"); //Dice picture
