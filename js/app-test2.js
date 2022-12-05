dbae88c0316c55ed55fc70bf198a4870cd119fc0
Kevin Bourgitteau <kevin.bourgitteau@gmail.com>
2022-12-05 13:44:09 +0100

    add app-test2 for new approach of program

---
+++ js/app-test2.js
@@ -0,0 +1,20 @@
+let players = [
+	{
+		playerNumber: 1,
+		name: "Player 1",
+		roundScore: 0,
+		totalScore: 0,
+		isPlaying: true,
+		isTheWinner: false,
+	},
+	{
+		playerNumber: 2,
+		name: "Player 2",
+		roundScore: 0,
+		totalScore: 0,
+		isPlaying: false,
+		isTheWinner: false,
+	},
+];
+
+let gameInProgress = null; // true / false
